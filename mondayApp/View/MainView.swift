5d1ea9b0a4ee79f080d193aaf57e96acae973371
Mehtab Gill <mehtabgill1907@gmail.com>
2020-09-06 02:54:08 +0100

    Create button animation

--- mondayApp/View/MainView.swift
+++ mondayApp/View/MainView.swift
@@ -52,7 +52,7 @@ struct MainView: View {
 }
 
 struct logginInNavBar : View {
-      
+  @State var open = false
   @Binding var selected : Int
   @State var expand = true //can use this to change size of bar
       
@@ -77,9 +77,22 @@ struct logginInNavBar : View {
                   }
                   Spacer(minLength: 15)
                       
-                  Button(action: {self.selected = 2}) {
-                      if (self.selected == 2) {Image("create").foregroundColor(.red)}
-                      else {Image("create").foregroundColor(.gray)}
+                  Button(action: {self.open.toggle()}) {
+                      Image(systemName: "plus")
+                          .rotationEffect(.degrees(open ? 45 : 0))
+                          .foregroundColor(.white)
+                          .font(.system(size: 20, weight: .bold))
+                          .animation(.spring(response: 0.2, dampingFraction: 0.4, blendDuration: 0))
+                  }
+                  .padding(15)
+                  .background(Color.red)
+                  .mask(Circle())
+                  .shadow(color: Color.red, radius: 1)
+                  .zIndex(1)
+                  
+                  SecondaryButton(open: $open, icon: "bubble.left.fill", color: "Blue", offsetY: -90)
+                  SecondaryButton(open: $open, icon: "trash", color: "Blue", offsetX: -60, offsetY: -60, delay: 0.2)
+                  SecondaryButton(open: $open, icon: "pencil", color: "Purple", offsetX: -90, delay: 0.4)
                   }
                   Spacer(minLength: 15)
                       
@@ -95,15 +108,36 @@ struct logginInNavBar : View {
                   }
 
               }
-              .padding(.vertical,self.expand ? 20 : 8)
-              .padding(.horizontal,self.expand ? 35 : 8)
+              .padding(.vertical,self.expand ? 5 : 5)
+              .padding(.horizontal,self.expand ? 5 : 5)
               .background(Color.white)
               .clipShape(Capsule())
-              .padding(22)
+              .padding(30)
               .padding(.bottom,30)
+              .edgesIgnoringSafeArea(.bottom)
           }
-      }.edgesIgnoringSafeArea(.bottom)
-          
+    }
+}
 
-      }
-  }
+struct SecondaryButton: View {
+    @Binding var open: Bool
+    var icon = "pencil"
+    var color = "Blue"
+    var offsetX = 0
+    var offsetY = 0
+    var delay = 0.0
+    
+    var body: some View {
+        Button(action: {}) {
+            Image(systemName: icon)
+                .foregroundColor(.blue)
+                .font(.system(size: 16, weight: .bold))
+        }
+        .padding()
+        .background(Color(color))
+        .mask(Circle())
+        .offset(x: open ? CGFloat(offsetX) : 0, y: open ? CGFloat(offsetY) : 0)
+        .scaleEffect(open ? 1: 0)
+        .animation(Animation.spring(response: 0.2, dampingFraction: 0.5, blendDuration: 0).delay(Double(delay)))
+    }
+}
