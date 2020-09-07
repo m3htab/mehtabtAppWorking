c9176270544cd86f2d394057d71d216c34fd9359
Mehtab Gill <mehtabgill1907@gmail.com>
2020-09-07 04:22:17 +0100

    need to fix the button and realign the tab bar

--- mondayApp/View/MainView.swift
+++ mondayApp/View/MainView.swift
@@ -15,7 +15,8 @@ struct MainView: View {
     @EnvironmentObject var appState: AppState
     @State var showNav = true
     @State var selected : Int
-    
+    @State var open = false
+
     var comeFrom : String
 
     var body: some View {
@@ -62,7 +63,7 @@ struct logginInNavBar : View {
           Spacer()
           HStack(alignment: .center){
               
-              Spacer(minLength: 0)
+              Spacer(minLength: 15)
               HStack(alignment: .bottom){
                   
                   Button(action: {self.selected = 0}) {
@@ -72,72 +73,61 @@ struct logginInNavBar : View {
                   Spacer(minLength: 15)
                   
                   Button(action: {self.selected = 1}) {
-                      if (self.selected == 1) {Image("activityActive").foregroundColor(.blue)}
+                      if (self.selected == 1) {Image("activityActive").foregroundColor(.red)}
                       else {Image("activity").foregroundColor(.gray)}
                   }
                   Spacer(minLength: 15)
-                      
+                    
+                
+                
+                
+                
+                
+                
                   Button(action: {self.open.toggle()}) {
                       Image(systemName: "plus")
-                          .rotationEffect(.degrees(open ? 45 : 0))
-                          .foregroundColor(.white)
-                          .font(.system(size: 20, weight: .bold))
-                          .animation(.spring(response: 0.2, dampingFraction: 0.4, blendDuration: 0))
-                  }
-                  .padding(15)
-                  .background(Color.red)
-                  .mask(Circle())
-                  .shadow(color: Color.red, radius: 1)
-                  .zIndex(1)
-                  
-                  SecondaryButton(open: $open, icon: "bubble.left.fill", color: "Blue", offsetY: -90)
-                  SecondaryButton(open: $open, icon: "trash", color: "Blue", offsetX: -60, offsetY: -60, delay: 0.2)
-                  SecondaryButton(open: $open, icon: "pencil", color: "Purple", offsetX: -90, delay: 0.4)
-                  }
+                       
+
+                    .rotationEffect(.degrees(open ? 45 : 0))
+                            .foregroundColor(.white)
+                            .font(.system(size: 20, weight: .bold))
+                            .animation(.spring(response: 0.2, dampingFraction: 0.4, blendDuration: 0))
+                    }
+                    .padding(15)
+                    .background(Color.red)
+                    .mask(Circle())
+                    .shadow(color: Color.red, radius: 1)
+                    .zIndex(1)
+
+
+    
+                
+                
+                
+                
                   Spacer(minLength: 15)
                       
                   Button(action: {self.selected = 3}) {
-                      if (self.selected == 3) {Image("userActive").foregroundColor(.blue)}
+                      if (self.selected == 3) {Image("userActive").foregroundColor(.red)}
                       else {Image("user").foregroundColor(.gray)}
                   }
                   Spacer(minLength: 15)
                       
                   Button(action: {self.selected = 4}) {
-                      if (self.selected == 4) {Image("messagesActive").foregroundColor(.blue)}
+                      if (self.selected == 4) {Image("messagesActive").foregroundColor(.red)}
                       else {Image("messages").foregroundColor(.gray)}
                   }
 
               }
-              .padding(.vertical,self.expand ? 5 : 5)
-              .padding(.horizontal,self.expand ? 5 : 5)
+              .padding(.vertical,self.expand ? 7 : 8)
+              .padding(.horizontal,self.expand ? 7: 8)
               .background(Color.white)
               .clipShape(Capsule())
-              .padding(30)
+              .padding(20)
               .padding(.bottom,30)
-              .edgesIgnoringSafeArea(.bottom)
-          }
-    }
-}
-
-struct SecondaryButton: View {
-    @Binding var open: Bool
-    var icon = "pencil"
-    var color = "Blue"
-    var offsetX = 0
-    var offsetY = 0
-    var delay = 0.0
-    
-    var body: some View {
-        Button(action: {}) {
-            Image(systemName: icon)
-                .foregroundColor(.blue)
-                .font(.system(size: 16, weight: .bold))
+              
+              }
+          }.edgesIgnoringSafeArea(.bottom)
+            
         }
-        .padding()
-        .background(Color(color))
-        .mask(Circle())
-        .offset(x: open ? CGFloat(offsetX) : 0, y: open ? CGFloat(offsetY) : 0)
-        .scaleEffect(open ? 1: 0)
-        .animation(Animation.spring(response: 0.2, dampingFraction: 0.5, blendDuration: 0).delay(Double(delay)))
     }
-}
